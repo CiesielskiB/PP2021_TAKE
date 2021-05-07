27e6150e85728c80bfb4839e0d4f3be50fa28cfe
BartoszC <Bartosz.Ciesielski1997@gmail.com>
2021-05-07 11:35:45 +0200

    Add MVC lab 8

---
+++ EnterpriseProjects/ASP.NET_MVC/MvcNews/MvcNews/Migrations/20210506113744_AddRowVersionMig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcNews.Migrations
+{
+    public partial class AddRowVersionMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "News",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "News");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
